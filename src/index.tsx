f134754b136244853f7cba5e1e0d7e0146b3f563
Bryan Oswaldo Juanacio <juanaciolalangui@gmail.com>
2020-08-26 21:50:01 -0500

    re-install

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import * as serviceWorker from "./serviceWorker";
-
-import { HashRouter as Router, Route } from "react-router-dom";
-import { Provider } from 'react-redux';
-
-import store from "./redux/store";
-
-import Init from "./pages/Init";
-import Game from "./pages/Game";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Router>
-    <Provider store={store}>
-    <React.StrictMode>
-      <Route exact path="/" component={Init} />
-      <Route exact path="/game" component={Game} />
-    </React.StrictMode>
-    </Provider>
-  </Router>,
-  document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-console.log('store', store.getState());
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
